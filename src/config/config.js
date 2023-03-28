bb844bc2ac4eb35f4ab08be26c1df4f87eb4935a
Jasowills <Jujuzhaddy01@gmail.com>
2023-03-28 21:00:20 +0100

    origin

--- src/config/config.js
+++ src/config/config.js
@@ -8,12 +8,13 @@ const databaseEnvDetails = {
   port: process.env.DB_CONFIG_PORT,
   dialect: 'postgres',
   logging: false,
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000
-  // }
+  pool: {
+  max: 10,
+  min: 1,
+  acquire: 3000,
+  idle: 5000
+}
+
 };
 
 const config = {
