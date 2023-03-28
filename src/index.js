8e0dc4620642a6c70e0d1db9bbfa5db15ceeb258
Jasowills <Jujuzhaddy01@gmail.com>
2023-03-28 22:22:38 +0100

    origin

--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(4002, () => console.log(`Server is running on port 4001`));
+app.listen(4005, () => console.log(`Server is running on port 4001`));
 
